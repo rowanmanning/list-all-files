3726744331f12f9586bdb52ffdd4c2189defba2c
Rowan Manning <accounts+github@rowanmanning.com>
2020-03-14 09:54:14 +0000

    Bump all development dependencies

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const assert = require('proclaim');
-const mockery = require('mockery');
 
 describe('index', () => {
 	let index;
+	let listAllFiles;
 
 	beforeEach(() => {
-		mockery.registerMock('./lib/list-all-files', 'mock-list-all-files');
 		index = require('../../index');
+		listAllFiles = require('../../lib/list-all-files');
 	});
 
 	it('aliases `lib/list-all-files`', () => {
-		assert.strictEqual(index, 'mock-list-all-files');
+		assert.strictEqual(index, listAllFiles);
 	});
 
 });
